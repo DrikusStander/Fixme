8cbbf07dc6b47374184ed5108509e9e0ff69890e
Hendrik STANDER <standerhc@live.com>
2018-08-10 17:37:26 +0200

    Some not working code

---
+++ Fixme/Fixme-market/src/main/java/com/hstander/market/Main.java
@@ -0,0 +1,14 @@
+package com.hstander.market;
+
+/**
+ * Hello world!
+ *
+ */
+// public class Main 
+// {
+//     public static void main( String[] args )
+//     {
+//         System.out.println( "Hello World!" );
+//     }
+// }
+
